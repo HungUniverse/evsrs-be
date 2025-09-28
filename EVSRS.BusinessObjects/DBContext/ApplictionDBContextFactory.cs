c173adabdd6474be29b2ce6155b5af739ab252e4
dangvibaominh-hub <maxmamoru16@gmail.com>
2025-09-28 16:50:38 +0700

    tesst 2

--- EVSRS.BusinessObjects/DBContext/ApplictionDBContextFactory.cs
+++ EVSRS.BusinessObjects/DBContext/ApplictionDBContextFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EVSRS.BusinessObjects.DBContext
 {
-    internal class ApplictionDBContextFactory
+    public class ApplictionDBContextFactory
     {
     }
 }
