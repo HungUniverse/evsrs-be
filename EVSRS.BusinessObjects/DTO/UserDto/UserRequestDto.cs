348010afd369d8546c1384617f50f104aa4c1260
HungUniverse <vuduchung060905@gmail.com>
2025-10-12 03:07:50 +0700

    Add CRUD for User

---
+++ EVSRS.BusinessObjects/DTO/UserDto/UserRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVSRS.BusinessObjects.DTO.UserDto
+{
+    public class UserRequestDto
+    {
+        public string? UserName { get; set; }
+        public string? UserEmail { get; set; }
+        public string? HashPassword { get; set; }
+        public string? Salt { get; set; }
+        public string? FullName { get; set; }
+        public string? ProfilePicture { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool IsVerify { get; set; } = false;
+    }
+}
--- EVSRS.BusinessObjects/Entity/ApplicationUser.cs
+++ EVSRS.BusinessObjects/Entity/ApplicationUser.cs
@@ -15,7 +15,7 @@ namespace EVSRS.BusinessObjects.Entity
         public string? HashPassword { get; set; }
         public string? Salt { get; set; }
         public string? FullName { get; set; }
-        public String? ProfilePicture { get; set; }
+        public string? ProfilePicture { get; set; }
         public string? DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; }
         public bool IsVerify { get; set; } = false;
